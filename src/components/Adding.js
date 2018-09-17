9b65afad41cf681a5847c4340d09afb2908f3a40
Sky <anvandp18@gmail.com>
2018-09-17 13:01:03 -0700

    create partial form for adding.js

--- src/components/Adding.js
+++ src/components/Adding.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
 
 class Adding extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			amount: '',
+			date: '',
+			details: ''
+		}
+	}
+
+	handleChange = (e) => {
+		console.log(this.state);
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
 	render() {
 		return(
-			<h1>Adding</h1>
+			<form className="AddingForm" onSubmit={this.handleSubmit}>
+				<label for="amount">Amount:</label>
+				<input type="number" name="amount" value={this.state.amount} onChange={this.handleChange} />
+				<button className="btn btn-primary" type="submit">Submit</button>
+			</form>
 		);
 	}
 }
