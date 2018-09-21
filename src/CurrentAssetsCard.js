37aa57f5742f67e3910d8b606932aabeea45ae74
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-09-21 11:21:31 -0700

    graphs display

---
+++ src/CurrentAssetsCard.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+import moment from 'moment';
+
+class AllMoneyList extends Component {
+	state = {
+		allMoney: []
+	}
+
+	// componentDidMount() {
+	// 	axios.get('http://localhost:3000/money/all')
+	// 		.then(res => {
+	// 			const allMoney = res.data;
+	// 			this.setState({ allMoney });
+	// 		})
+	// }
+
+	componentDidMount() {
+		axios.post('http://localhost:3000/money/all/post', this.props.user)
+			.then(res => {
+				console.log("ASSETS DATA", res.data);
+				const allMoney = res.data;
+				this.setState({ allMoney });
+			})
+	}
+
+	render() {
+
+		 const income = [];
+		 var totalIncome = 0;
+
+		 this.state.allMoney.forEach(i =>{
+      		 { income.push(i.amount) }
+      		})
+
+      	income.reduce((total, inc) => totalIncome = total + inc, 0);
+
+		return(
+			
+			<div>
+
+				<h2>{`Monthly Income to Date: $${totalIncome}`}</h2>
+
+			</div>
+			
+
+
+		);
+	}
+}
+
+export default AllMoneyList;
