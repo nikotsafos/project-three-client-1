e4dbcfd1390f717b093d0b77291c90364b8a2f93
Sky <anvandp18@gmail.com>
2018-09-21 17:02:59 -0700

    fixing the spending money

--- src/components/SpendingDetails.js
+++ src/components/SpendingDetails.js
@@ -49,6 +49,35 @@ export default class SpendingDetails extends Component {
             break;
 }
 
+    let spending = this.props.spendingFunction();
+ 
+      const spendingItems12 = (spending) => {
+      let arr12 = []
+      for (let key in spending["December"]){
+      arr12.push({month: "December", data: {key: key, data: spending['December'][key]}})
+    }
+      console.log('array of result: ', arr12);
+      return arr12
+    }
+
+      const spendingItems11 = (spending) => {
+      let arr11 = []
+      for (let key in spending["November"]){
+      arr9.push({month: "November", data: {key: key, data: spending['November'][key]}})
+    }
+      console.log('array of result: ', arr11);
+      return arr11
+    }
+
+      const spendingItems10 = (spending) => {
+      let arr10 = []
+      for (let key in spending["October"]){
+      arr10.push({month: "October", data: {key: key, data: spending['October'][key]}})
+    }
+      console.log('array of result: ', arr10);
+      return arr10
+    }
+
 
     let spending = this.props.spendingFunction();
       const spendingItems9 = (spending) => {
@@ -68,6 +97,63 @@ export default class SpendingDetails extends Component {
       console.log('array of result: ', arr8);
       return arr8
     }
+
+       const spendingItems7 = (spending) => {
+      let arr7 = []
+      for (let key in spending["July"]){
+      arr7.push({month: "July", data: {key: key, data: spending['July'][key]}})
+    }
+      return arr7
+    }
+
+     const spendingItems6 = (spending) => {
+      let arr6 = []
+      for (let key in spending["June"]){
+      arr6.push({month: "June", data: {key: key, data: spending['June'][key]}})
+    }
+      return arr6
+    }
+
+     const spendingItems5 = (spending) => {
+      let arr5 = []
+      for (let key in spending["May"]){
+      arr5.push({month: "May", data: {key: key, data: spending['May'][key]}})
+    }
+      return arr5
+    }
+
+     const spendingItems4 = (spending) => {
+      let arr4 = []
+      for (let key in spending["April"]){
+      arr4.push({month: "April", data: {key: key, data: spending['April'][key]}})
+    }
+      return arr4
+    }
+
+     const spendingItems3 = (spending) => {
+      let arr3 = []
+      for (let key in spending["March"]){
+      arr3.push({month: "March", data: {key: key, data: spending['March'][key]}})
+    }
+      return arr3
+    }
+
+     const spendingItems2 = (spending) => {
+      let arr2 = []
+      for (let key in spending["February"]){
+      arr2.push({month: "February", data: {key: key, data: spending['February'][key]}})
+    }
+      return arr2
+    }
+
+     const spendingItems1 = (spending) => {
+      let arr1 = []
+      for (let key in spending["January"]){
+      arr1.push({month: "January", data: {key: key, data: spending['January'][key]}})
+    }
+      return arr1
+    }
+
 
         // items += (<h3>key</h3>)
         // console.log(key)
