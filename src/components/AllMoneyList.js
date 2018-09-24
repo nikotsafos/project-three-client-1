71358d3140752b9802a9508e4dfc17aae431ef12
Sky <anvandp18@gmail.com>
2018-09-23 18:19:17 -0700

    finish showing adding money on September and August

--- src/components/AllMoneyList.js
+++ src/components/AllMoneyList.js
@@ -6,7 +6,8 @@ import SERVER_URL from '../constants/server';
 
 class AllMoneyList extends Component {
 	state = {
-		allMoney: []
+		allMoney: [],
+
 	}
 
 	// componentDidMount() {
@@ -29,19 +30,49 @@ class AllMoneyList extends Component {
 		})
 		.then(res => {
 				const allMoney = res.data;
+				console.log('All Money in database ',allMoney);
 				this.setState({ allMoney });
+				
 			})
 	}
 
 	render() {
+		const moneyList = {
+			September: [],
+			August: []
+		};
+		const monthJSX = [];
+		
+		this.state.allMoney.map( money => {
+			if (moment(money.date).format('MMMM') === 'September') {
+				moneyList.September.push(money);
+			}else if (moment(money.date).format('MMMM') === 'August') {
+				moneyList.August.push(money);
+			}
+		});
 
+			console.log('List of money adding: ', moneyList);
+		
+
+		monthJSX[0] = moneyList.September.map(money => {
+			return(
+				<p>
+						Money from: {money.description}. Amount: {money.amount}. Date: {moment(money.date).calendar()}
+						<button>Edit</button><button>Delete</button>
+						</p>
+						)})
+		monthJSX[1] = moneyList.August.map(money => {
+			return(
+				<p>
+						Money from: {money.description}. Amount: {money.amount}. Date: {moment(money.date).calendar()}
+						<button>Edit</button><button>Delete</button>
+						</p>
+						)})
 
 		return(
 			<div>
 				<div>
-
 				<Adding user={this.props.user} />
-
 			</div>
 			<div>
 
@@ -50,7 +81,16 @@ class AllMoneyList extends Component {
 						Money from: {allMoney.description}. Amount: {allMoney.amount}. Date: {moment(allMoney.date).calendar()}
 						<button>Edit</button><button>Delete</button>
 						</p>)}
+
+				
+						
 				</ul>
+
+				<p>September Adding</p>
+				{monthJSX[0]}
+				<p>August Adding</p>
+				{monthJSX[1]}
+				<br/><br/><br/><br/>
 			</div>
 
    </div>
